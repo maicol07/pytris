d848edc0560a74fa544ab2052f79f3cc32d9e0d7
Maicol <maicolbattistini@live.it>
2017-05-11 21:49:40 +0200

    Update tris.py
    *Ottimizzazione, miglioramento e aggiornamento del codice
    *Aggiunto controllo esistenza del file tabellone.txt (riga 156)
    *Scrittura nel file di una partita della modalità 1
    PERFETTAMENTE FUNZIONANTE LA MODALITA' 1!! MODALITA' 2 E 3 ANCORA IN SVILUPPO

--- tris.py
+++ tris.py
@@ -1,6 +1,20 @@
+###########################
+#                         #
+#           TRIS          #
+#    written in Python    #
+#          v.1.1          #
+#                         #
+###########################
+
 from random import randint
-def aggiungiMossa(mossa,mossasucc): #mossa=(0,1)
-    
+import os.path
+def vintoono(vincitore):
+    if (vincitore=="g1"):
+        return "G1"
+    elif (vincitore=="g2"):
+        return "G2"
+    return ""
+def aggiungiMossa(m,mossa,mossasucc): #funzionante
     if (mossasucc=="g1"):
         simbolo="O"
         m[int(mossa[0])][int(mossa[1])]=simbolo
@@ -8,117 +22,142 @@ def aggiungiMossa(mossa,mossasucc): #mossa=(0,1)
         simbolo="X"
         m[int(mossa[0])][int(mossa[1])]=simbolo
 
-def creatabellone(m):
+def creatabellone(m): #funzionante
     cont=0
     print("\t",cont,"\t",cont+1,"\t",cont+2)
     for i in m:
-        print(cont, end="\t")
+        print(cont,end="\t")
         for d in i:
           print(d,end="\t")
         cont=cont+1
         print()
     print()
-f=open(r"tabellone.txt","w")
-f.close()
-f=open(r"tabellone.txt","r+")
-m=[[0,0,0],[0,0,0],[0,0,0]]
-global m
-f.write(str(m))
-def vittoriaono(m,mossasucc):
-    if(m[0][0]==m[0][1]==m[0][2]):
-        vittoria=True
+def vittoriaono(m,mossasucc): #...
+    simbolovitt=""
+    if ((m[0][0]=="X" and m[0][1]=="X" and m[0][2]=="X") or
+        (m[0][0]=="O" and m[0][1]=="O" and m[0][2]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[1][0]==m[1][1]==m[1][2]):
-        vittoria=True
+    if ((m[1][0]=="X" and m[1][1]=="X" and m[1][2]=="X") or
+        (m[1][0]=="O" and m[1][1]=="O" and m[1][2]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[1][0]==m[1][1]==m[1][2]):
-        vittoria=True
+    if ((m[2][0]=="X" and m[2][1]=="X" and m[2][2]=="X") or
+        (m[2][0]=="O" and m[2][1]=="O" and m[2][2]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[0][0]==m[1][0]==m[2][0]):
-        vittoria=True
+    if ((m[0][0]=="X" and m[1][0]=="X" and m[2][0]=="X") or
+        (m[1][0]=="O" and m[1][0]=="O" and m[2][0]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[1][0]==m[1][1]==m[1][2]):
-        vittoria=True
+    if ((m[0][1]=="X" and m[1][1]=="X" and m[2][1]=="X") or
+        (m[0][1]=="O" and m[1][1]=="O" and m[2][1]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[2][0]==m[2][1]==m[2][2]):
-        vittoria=True
+    if ((m[2][0]=="X" and m[2][1]=="X" and m[2][2]=="X") or
+        (m[2][0]=="O" and m[2][1]=="O" and m[2][2]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[0][0]==m[1][1]==m[2][2]):
-        vittoria=True
+    if ((m[0][0]=="X" and m[1][1]=="X" and m[2][2]=="X") or
+        (m[0][0]=="O" and m[1][1]=="O" and m[2][2]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
-    if(m[0][2]==m[1][1]==[2][0]):
-        vittoria=True
+    if ((m[0][2]=="X" and m[1][1]=="X" and m[2][0]=="X") or
+        (m[0][2]=="O" and m[1][1]=="O" and m[2][0]=="O")):
         if (mossasucc=="g2"):
             simbolovitt="X"
         else:
             simbolovitt="O"
     return simbolovitt
-    
-def modalita1():
-    vittoria=False
-    global vittoria
-    while (vittoria!=True):
+def modalita1(g1,g2,f): #....
+    m=[["","",""],["","",""],["","",""]]
+    simbolovitt="S"
+    v="G0"
+    vincitore="G0"
+    while (v!="G1" or v!="G2"):
         creatabellone(m)
-        if m==[[0,0,0],[0,0,0],[0,0,0]]:
+        if m==[["","",""],["","",""],["","",""]]:
           if (randint(0,1)==0):
             mossa1X=int(input("Giocatore 1, inserisci la riga dove vuoi fare la tua mossa: "))
             mossa1Y=int(input("Giocatore 1, inserisci la colonna dove vuoi fare la tua mossa: "))
             mossa1=(mossa1X,mossa1Y)
             mossasucc="g2"
-            aggiungiMossa(mossa1,mossasucc)
+            while (m[mossa1X][mossa1Y]=="O" or m[mossa1X][mossa1Y]=="X"):
+                print("NON PUOI SOVRASCRIVERE UN SIMBOLO GIA' ESISTENTE!!")
+                mossa1X=int(input("Giocatore 1, inserisci la riga dove vuoi fare la tua mossa: "))
+                mossa1Y=int(input("Giocatore 1, inserisci la colonna dove vuoi fare la tua mossa: "))
+                mossa1=(mossa1X,mossa1Y)
+            aggiungiMossa(m,mossa1,mossasucc)
           else:
             mossa2X=int(input("Giocatore 2, inserisci la riga dove vuoi fare la tua mossa: "))
             mossa2Y=int(input("Giocatore 2, inserisci la colonna dove vuoi fare la tua mossa: "))            
             mossa2=(mossa2X,mossa2Y)
             mossasucc="g1"
-            aggiungiMossa(mossa2,mossasucc)
+            while (m[mossa2X][mossa2Y]=="O" or m[mossa2X][mossa2Y]=="X"):
+                print("NON PUOI SOVRASCRIVERE UN SIMBOLO GIA' ESISTENTE!!")
+                mossa2X=int(input("Giocatore 2, inserisci la riga dove vuoi fare la tua mossa: "))
+                mossa2Y=int(input("Giocatore 2, inserisci la colonna dove vuoi fare la tua mossa: "))            
+                mossa2=(mossa2X,mossa2Y)
+            else:
+                aggiungiMossa(m,mossa2,mossasucc)
         else:
             if (mossasucc=="g1"):
                 mossa1X=int(input("Giocatore 1, inserisci la riga dove vuoi fare la tua mossa: "))
                 mossa1Y=int(input("Giocatore 1, inserisci la colonna dove vuoi fare la tua mossa: "))
                 mossa1=(mossa1X,mossa1Y)
                 mossasucc="g2"
-                aggiungiMossa(mossa1,mossasucc)
+                while (m[mossa1X][mossa1Y]=="O" or m[mossa1X][mossa1Y]=="X"):
+                    print("NON PUOI SOVRASCRIVERE UN SIMBOLO GIA' ESISTENTE!!")
+                    mossa1X=int(input("Giocatore 1, inserisci la riga dove vuoi fare la tua mossa: "))
+                    mossa1Y=int(input("Giocatore 1, inserisci la colonna dove vuoi fare la tua mossa: "))
+                    mossa1=(mossa1X,mossa1Y)
+                aggiungiMossa(m,mossa1,mossasucc)
             else:
                 mossa2X=int(input("Giocatore 2, inserisci la riga dove vuoi fare la tua mossa: "))
                 mossa2Y=int(input("Giocatore 2, inserisci la colonna dove vuoi fare la tua mossa: "))
                 mossa2=(mossa2X,mossa2Y)
                 mossasucc="g1"
-                aggiungiMossa(mossa2,mossasucc)
+                while (m[mossa2X][mossa2Y]=="O" or m[mossa2X][mossa2Y]=="X"):
+                    print("NON PUOI SOVRASCRIVERE UN SIMBOLO GIA' ESISTENTE!!")
+                    mossa2X=int(input("Giocatore 2, inserisci la riga dove vuoi fare la tua mossa: "))
+                    mossa2Y=int(input("Giocatore 2, inserisci la colonna dove vuoi fare la tua mossa: "))            
+                    mossa2=(mossa2X,mossa2Y)
+                aggiungiMossa(m,mossa2,mossasucc)
         simbolovitt=vittoriaono(m,mossasucc)
         if (simbolovitt=="X"):
             vincitore="g1"
-            vittoria=True
-            print("G1 HA VINTO!!")
-        elif (simbolovitt=="O"):
+        if (simbolovitt=="O"):
             vincitore="g2"
-            vittoria=True
-            print("G2 HA VINTO!!")
-    
-simbolovitt="L"
-global simbolovitt
-  
+        v=vintoono(vincitore)
+        if (v=="G1"):
+            print("\n","CONGRATULAZIONI GIOCATORE 1!!! HAI VINTO!!!!")
+            break
+        if (v=="G2"):
+            print("\n","CONGRATULAZIONI GIOCATORE 2!!! HAI VINTO!!!!")
+            break
+    print()
+    f.write("m")
+    return "PARTITA TERMINATA"
+##### APERTURA FILE ECC.. #####
+if not(os.path.exists(r"tabellone.txt")):
+    f=open(r"tabellone.txt","w")
+    f.close()
+f=open(r"tabellone.txt","r+")
+##### MENU PROGRAMMA #####
 print("BENVENUTO IN TRIS SIMULATOR!")
 scelta=-1
 while (scelta!=-2):
@@ -126,12 +165,10 @@ while (scelta!=-2):
     scelta=int(input("Inserisci la tua scelta: "))
     if scelta==0:
         g1="X"
-        global g1
         g2="O"
-        global g2
-        print(modalita1())
-        
+        print(modalita1(g1,g2,f))
     if (scelta==1):
         print(modalita2())
     if (scelta==2):
         print(modalita3())
+f.close()
